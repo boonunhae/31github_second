fabc872be9dc559d750bcf381787c8dd388be7d0
boonunhae <boonunhae@users.noreply.github.com>
2023-12-06 16:04:29 +0600

    cfqn yf gfqnjyt bkb buhf

--- python1.py
+++ python1.py
@@ -1,14 +1,3 @@
-class Person:
-    pass 
-        def say_hello(self):
-            print("Hi, my dear friend!")
-    Nargiza = Person()
-    Sanjar = Person()
-    Nargiza.say_hello()
-
-new_func()
-
-
 
 
 
